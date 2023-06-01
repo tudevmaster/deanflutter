81feb50db0866c8e92f376350cb24cd7e464a1ba
tudevmaster <dangthanhtu502@gmail.com>
2023-06-01 20:52:08 +0700

    Tú

--- shopmaytinh/lib/pages/home_screen.dart
+++ shopmaytinh/lib/pages/home_screen.dart
@@ -44,8 +44,127 @@ class HomeScreen extends StatelessWidget {
                   end: Alignment.bottomCenter,
                 ),
               ),
+              child: Center(
+                child: Text(
+                  "Dear Pro",
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
             ),
           ),
+          Positioned(
+            right: -getBigDiamter(context) / 2,
+            bottom: -getBigDiamter(context) / 2,
+            child: Container(
+              width: getBigDiamter(context),
+              height: getBigDiamter(context),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Color(0xFFF3E9EE),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: ListView(
+              children: [
+                Container(
+                  margin: EdgeInsets.fromLTRB(20, 20, 20, 10),
+                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      icon: Icon(
+                        Icons.vpn_key,
+                        color: Color(0xFFFF4891),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey.shade100),
+                      ),
+                      labelText: "Email",
+                      enabledBorder: InputBorder.none,
+                      labelStyle: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(0, 10, 20, 10),
+                    child: Text(
+                      "Quên Mật Khẩu ?",
+                      style: TextStyle(
+                        color: Color(0xFFFF4891),
+                        fontSize: 12,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(20, 20, 20, 30),
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    gradient: LinearGradient(
+                      colors: [Color(0xFFB226B2), Color(0xFFFF6DA7)],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: Material(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.transparent,
+                    child: InkWell(
+                      onTap: () {},
+                      borderRadius: BorderRadius.circular(20),
+                      splashColor: Colors.amber,
+                      child: Center(
+                        child: Text(
+                          "Đăng Nhập",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Chưa có tài khoản ?",
+                      style: TextStyle(
+                        fontSize: 11,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text(
+                      "Chưa có tài khoản ?",
+                      style: TextStyle(
+                        fontSize: 11,
+                        color: Color(0xFFFF4891),
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
